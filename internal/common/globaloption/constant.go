76637d25a07a40680b86e8510e54b95c56d5c780
kageyamountain <kageyamountain@users.noreply.github.com>
2026-05-09 10:47:09 +0900

    subccomand 実装

---
+++ internal/common/globaloption/constant.go
@@ -0,0 +1,9 @@
+package globaloption
+
+const (
+	OptionNameRepositoryOwner string = "owner"
+	OptionNameRepositoryName  string = "name"
+	OptionNameCreatedAtFrom   string = "from"
+	OptionNameCreatedAtTo     string = "to"
+	OptionNameIgnoreLabels    string = "ignore-labels"
+)
---
+++ internal/common/globaloption/option_ignore_labels.go
@@ -0,0 +1,50 @@
+package globaloption
+
+import (
+	"errors"
+	"strings"
+)
+
+type IgnoreLabel string
+
+func parseIgnoreLabel(v string) (IgnoreLabel, error) {
+	trimmed := strings.TrimSpace(v)
+	if trimmed == "" {
+		return "", errors.New("empty ignore label is not allowed")
+	}
+	return IgnoreLabel(trimmed), nil
+}
+
+func (i IgnoreLabel) String() string {
+	return string(i)
+}
+
+type IgnoreLabels []IgnoreLabel
+
+func ParseIgnoreLabels(v string) (IgnoreLabels, error) {
+	if v == "" {
+		return IgnoreLabels{}, nil
+	}
+
+	values := strings.Split(v, ",")
+	ignoreLabels := make([]IgnoreLabel, 0, len(values))
+	for _, value := range values {
+		ignoreLabel, err := parseIgnoreLabel(value)
+		if err != nil {
+			return nil, err
+		}
+
+		ignoreLabels = append(ignoreLabels, ignoreLabel)
+	}
+
+	return ignoreLabels, nil
+}
+
+func (i IgnoreLabels) Contains(labelName string) bool {
+	for _, label := range i {
+		if label.String() == labelName {
+			return true
+		}
+	}
+	return false
+}
